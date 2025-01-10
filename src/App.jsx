a6494d5673c5b34ce57fbd8379ceb3d22d61c59b
durgeshyadav21 <durgesh@poxscan.io>
2025-01-10 13:48:47 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+     <p className="text-4xl font-bold text-blue-600">Hello World</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+
+      fontFamily: {
+
+      },
+
+      backgroundImage: {
+        "yellow-gradient":"linear-gradient(to bottom, #FAAB02 0%, #539D43 100%)",
+
+      },   
+    },
+  },
+  plugins: [],
+}
