863809272430751b9f7e4cade95552bca27bf6f0
durgeshyadav21 <durgesh@poxscan.io>
2025-04-02 09:38:14 +0530

    code push

---
+++ src/components/resuableComponent/MapComponent.jsx
@@ -0,0 +1,23 @@
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+const containerStyle = {
+    width: "100%",
+    height: "300px",
+};
+
+const center = {
+    lat: 37.7749, // Replace with your company's latitude
+    lng: -122.4194, // Replace with your company's longitude
+};
+
+const MapComponent = () => {
+    return (
+        <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
+            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+                <Marker position={center} />
+            </GoogleMap>
+        </LoadScript>
+    );
+};
+
+export default MapComponent;
