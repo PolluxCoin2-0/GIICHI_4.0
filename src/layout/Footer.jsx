b6f4301c33617e80531ea623106a0b75151e40c0
durgeshyadav21 <durgesh@poxscan.io>
2025-04-02 10:18:47 +0530

    ADDED MAP

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -16,60 +16,69 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row justify-between">
         {/* Logo */}
         <div>
-          <img src={giichiLogo} alt="Giichi-logo" className="w-48 md:w-20 lg:w-40 xl:w-full object-contain cursor-pointer" />
+          <img
+            src={giichiLogo}
+            alt="Giichi-logo"
+            className="w-48 md:w-20 lg:w-40 xl:w-full object-contain cursor-pointer"
+          />
 
           <div className="mt-12">
-          <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
-            FOLLOW
-          </p>
-          <div className="flex flex-row space-x-5 pt-6">
-            <Link
-              to="https://x.com/GiichiPvt?t=7FutgqPG6G_2umfDR53SgQ&s=09"
-              target="_blank" rel="noopener noreferrer"
-            >
-              <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
-                <FaXTwitter size={24} color="#000000" />
-              </p>
-            </Link>
+            <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
+              FOLLOW
+            </p>
+            <div className="flex flex-row space-x-5 pt-6">
+              <Link
+                to="https://x.com/GiichiPvt?t=7FutgqPG6G_2umfDR53SgQ&s=09"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
+                  <FaXTwitter size={24} color="#000000" />
+                </p>
+              </Link>
 
-            <Link
-              to="https://www.linkedin.com/company/giichi-it-solution/"
-              target="_blank" rel="noopener noreferrer"
-            >
-              <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
-                <FaLinkedinIn size={24} color="#000000" />
-              </p>
-            </Link>
+              <Link
+                to="https://www.linkedin.com/company/giichi-it-solution/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
+                  <FaLinkedinIn size={24} color="#000000" />
+                </p>
+              </Link>
 
-            <Link
-              to="https://www.instagram.com/giichi_labs/?igsh=MWhhY3RpMmtpeHBveA%3D%3D#"
-              target="_blank" rel="noopener noreferrer"
-            >
-              <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
-                <FaInstagram size={24} color="#000000" />
-              </p>
-            </Link>
-          </div>
+              <Link
+                to="https://www.instagram.com/giichi_labs/?igsh=MWhhY3RpMmtpeHBveA%3D%3D#"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
+                  <FaInstagram size={24} color="#000000" />
+                </p>
+              </Link>
+            </div>
 
-          <div className="flex flex-row space-x-5 pt-6">
-            <Link
-              to="https://x.com/GiichiPvt?t=7FutgqPG6G_2umfDR53SgQ&s=09"
-              target="_blank" rel="noopener noreferrer"
-            >
-              <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
-                <FaYoutube size={24} color="#000000" />
-              </p>
-            </Link>
+            <div className="flex flex-row space-x-5 pt-6">
+              <Link
+                to="https://x.com/GiichiPvt?t=7FutgqPG6G_2umfDR53SgQ&s=09"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
+                  <FaYoutube size={24} color="#000000" />
+                </p>
+              </Link>
 
-            <Link
-              to="https://blog.giichi.network/"
-              target="_blank" rel="noopener noreferrer"
-            >
-              <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
-                <RiMediumFill size={24} color="#000000" />
-              </p>
-            </Link>
-             </div>
+              <Link
+                to="https://blog.giichi.network/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="px-3 md:px-2 lg:px-3 py-3 md:py-2 lg:py-3 rounded-full bg-yellow-gradient transform hover:scale-110 transition-transform duration-300">
+                  <RiMediumFill size={24} color="#000000" />
+                </p>
+              </Link>
+            </div>
           </div>
         </div>
 
@@ -78,86 +87,108 @@ const Footer = () => {
           <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
             QUICK LINKS
           </p>
-          <a href="https://www.giichi.network/about-us" >
-          <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-           About Us
-          </p>
+          <a href="https://www.giichi.network/about-us">
+            <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
+              About Us
+            </p>
           </a>
-          <a href="https://blog.giichi.network/" >
-          <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-          Insights
-          </p>
+          <a href="https://blog.giichi.network/">
+            <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
+              Insights
+            </p>
           </a>
           <a href="https://www.giichi.network/blockchain-development">
-          <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-          Blockchain
-          </p>
+            <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
+              Blockchain
+            </p>
           </a>
-          <a href="https://www.giichi.network/ai-ml-solution" >
-          <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-          Artificial Intelligence
-          </p>
+          <a href="https://www.giichi.network/ai-ml-solution">
+            <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
+              Artificial Intelligence
+            </p>
           </a>
           <a href="/">
-          <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-          Web3 Consulting
-          </p>
+            <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
+              Web3 Consulting
+            </p>
           </a>
           <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-          Crypto Solutions
+            Crypto Solutions
           </p>
           <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-          Asset Tokenization
+            Asset Tokenization
           </p>
-          <a href="https://blog.giichi.network/" target="_blank" rel="noopener noreferrer">
-          <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-           Blogs
-          </p>
+          <a
+            href="https://blog.giichi.network/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
+              Blogs
+            </p>
           </a>
           <a href="https://www.giichi.network/contact-us">
-          <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
-           Contact
-          </p>
+            <p className="text-white text-opacity-70 hover:text-white pt-4 lg:pt-6 text-xs md:text-[11px] lg:text-xs xl:text-[15px] cursor-pointer">
+              Contact
+            </p>
           </a>
         </div>
 
         {/* Products */}
         <div>
           <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
-          CONNECT
+            CONNECT
           </p>
           <div className="mt-8">
-          <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
-          Sales Enquiry
-          </p>
-          <div className="flex flex-row items-center space-x-4 mt-4">
-          <IoCall color="#ffffff"/>
-          <p className="text-white text-opacity-70 text-xs md:text-[12px] lg:text-xs xl:text-[15px] font-normal ">+91 92664 16198</p>
+            <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
+              Sales Enquiry
+            </p>
+            <div className="flex flex-row items-center space-x-4 mt-4">
+              <IoCall color="#ffffff" />
+              <p className="text-white text-opacity-70 text-xs md:text-[12px] lg:text-xs xl:text-[15px] font-normal ">
+                +91 92664 16198
+              </p>
+            </div>
           </div>
-         </div>
-         <div className="mt-8">
-          <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
-          HR Enquiry
-          </p>
-          <div className="flex flex-row items-center space-x-4 mt-4">
-          <IoCall color="#ffffff"/>
-          <p className="text-white text-opacity-70 text-xs md:text-[12px] lg:text-xs xl:text-[15px] font-normal ">+91 92664 16198</p>
+          <div className="mt-8">
+            <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
+              HR Enquiry
+            </p>
+            <div className="flex flex-row items-center space-x-4 mt-4">
+              <IoCall color="#ffffff" />
+              <p className="text-white text-opacity-70 text-xs md:text-[12px] lg:text-xs xl:text-[15px] font-normal ">
+                +91 92664 16198
+              </p>
+            </div>
           </div>
-         </div>
-         <div className="mt-8">
-          <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
-          Email
-          </p>
-          <div className="flex flex-row items-center space-x-4 mt-4">
-          <IoMailOpenSharp color="#ffffff"/>
-          <p className="text-white text-opacity-70 text-xs md:text-[12px] lg:text-xs xl:text-[15px] font-normal">info@giichi.network</p>
+          <div className="mt-8">
+            <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
+              Email
+            </p>
+            <div className="flex flex-row items-center space-x-4 mt-4">
+              <IoMailOpenSharp color="#ffffff" />
+              <p className="text-white text-opacity-70 text-xs md:text-[12px] lg:text-xs xl:text-[15px] font-normal">
+                info@giichi.network
+              </p>
+            </div>
           </div>
-         </div>
-      </div>
+        </div>
 
-        {/* Socials Icons*/}
+        {/* Our Location */}
         <div>
-       
+          <p className="text-white font-semibold text-sm lg:text-[17px] pt-6 md:pt-0">
+            LOCATION
+          </p>
+          <div className="mt-6">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345094246!2d144.95592831550496!3d-37.81720997975167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf5778fd78f5c4a99!2sFederation+Square!5e0!3m2!1sen!2sau!4v1614325275563!5m2!1sen!2sau"
+              width="100%"
+              height="150"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+            ></iframe>
+          </div>
         </div>
       </div>
 
